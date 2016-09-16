94681a8efa393487276c4837994903eb2cccc72e
grammophone <emmanuel.liossis@gmail.com>
2016-09-16 21:30:17 +0300

    Create session scaffold.

---
+++ AccountingSession.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Grammophone.DataAccess;
+using Grammophone.Domos.DataAccess;
+using Grammophone.Domos.Domain;
+using Grammophone.Domos.Domain.Accounting;
+using Grammophone.Domos.Domain.Workflow;
+
+namespace Grammophone.Domos.Accounting
+{
+	/// <summary>
+	/// An <see cref="IDisposable"/> session for accounting actions. 
+	/// CAUTION: All actions taking entities as parameters
+	/// should have the entities connected via the <see cref="DomainContainer"/> of the class.
+	/// </summary>
+	/// <typeparam name="U">
+	/// The type of users, derived from <see cref="User"/>.
+	/// </typeparam>
+	/// <typeparam name="ST">
+	/// The type of state transitions, derived from <see cref="StateTransition{U}"/>.
+	/// </typeparam>
+	/// <typeparam name="A">The type of accounts, derived from <see cref="Account{U}"/>.</typeparam>
+	/// <typeparam name="P">The type of the postings, derived from <see cref="Posting{U, A}"/>.</typeparam>
+	/// <typeparam name="R">The type of remittances, derived from <see cref="Remittance{U, A}"/>.</typeparam>
+	/// <typeparam name="J">
+	/// The type of accounting journals, derived from <see cref="Journal{U, ST, A, P, R}"/>.
+	/// </typeparam>
+	/// <typeparam name="D">The type of domain container for entities.</typeparam>
+	public class AccountingSession<U, ST, A, P, R, J, D> : IDisposable
+		where U : User
+		where ST : StateTransition<U>
+		where A : Account<U>
+		where P : Posting<U, A>
+		where R : Remittance<U, A>
+		where J : Journal<U, ST, A, P, R>
+		where D : class, IDomosDomainContainer<U, ST, A, P, R, J>
+	{
+		#region Private classes
+
+		/// <summary>
+		/// Entity listener to set the fields
+		/// of entities of type <see cref="ITrackingEntity"/>
+		/// or <see cref="IUserTrackingEntity"/>.
+		/// </summary>
+		private class EntityListener : IEntityListener
+		{
+			#region Private fields
+
+			/// <summary>
+			/// The ID of the acting user.
+			/// </summary>
+			private long agentID;
+
+			#endregion
+
+			#region Construction
+
+			/// <summary>
+			/// Create.
+			/// </summary>
+			/// <param name="agentID">The ID of the acting user.</param>
+			public EntityListener(long agentID)
+			{
+				this.agentID = agentID;
+			}
+
+			#endregion
+
+			#region Public methods
+
+			public void OnAdding(object entity)
+			{
+				MarkAsModified(entity);
+			}
+
+			public void OnChanging(object entity)
+			{
+				MarkAsModified(entity);
+			}
+
+			public void OnDeleting(object entity)
+			{
+				// NOP.
+			}
+
+			public void OnRead(object entity)
+			{
+				// NOP.
+			}
+
+			#endregion
+
+			#region Private methods
+
+			/// <summary>
+			/// If the entity is of type <see cref="ITrackingEntity"/>
+			/// or <see cref="IUserTrackingEntity"/>, sets the corresponding fields.
+			/// </summary>
+			/// <param name="entity">The entity to handle.</param>
+			private void MarkAsModified(object entity)
+			{
+				var trackingEntity = entity as ITrackingEntity;
+
+				if (trackingEntity == null) return;
+
+				DateTime now = DateTime.UtcNow;
+
+				trackingEntity.LastModifierUserID = agentID;
+				trackingEntity.LastModificationDate = now;
+
+				if (trackingEntity.CreatorUserID == 0L)
+				{
+					trackingEntity.CreatorUserID = agentID;
+				}
+
+				trackingEntity.CreationDate = now;
+
+				var userTrackingEntity = entity as IUserTrackingEntity;
+
+				if (userTrackingEntity != null)
+				{
+					if (userTrackingEntity.OwningUserID == 0L)
+					{
+						userTrackingEntity.OwningUserID = agentID;
+					}
+				}
+			}
+
+			#endregion
+		}
+
+		#endregion
+
+		#region Private fields
+
+		private ICollection<IEntityListener> preexistingEntityListeners;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="domainContainer">The entities domain container.</param>
+		/// <param name="agent">The acting user.</param>
+		public AccountingSession(D domainContainer, U agent)
+		{
+			if (domainContainer == null) throw new ArgumentNullException(nameof(domainContainer));
+			if (agent == null) throw new ArgumentNullException(nameof(agent));
+
+			Initialize(domainContainer, agent);
+		}
+
+		/// <summary>
+		/// Create.
+		/// </summary>
+		/// <param name="domainContainer">The entities domain container.</param>
+		/// <param name="agentPickPredicate">A predicate to select a user.</param>
+		public AccountingSession(D domainContainer, Expression<Func<U, bool>> agentPickPredicate)
+		{
+			if (domainContainer == null) throw new ArgumentNullException(nameof(domainContainer));
+			if (agentPickPredicate == null) throw new ArgumentNullException(nameof(agentPickPredicate));
+
+			U agent = domainContainer.Users.FirstOrDefault();
+
+			if (agent == null)
+				throw new ArgumentException("The specified user does not exist.", nameof(agentPickPredicate));
+
+			Initialize(domainContainer, agent);
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The domain container used in the session.
+		/// </summary>
+		public D DomainContainer { get; private set; }
+
+		/// <summary>
+		/// The user operating the session actions.
+		/// </summary>
+		public U Agent { get; private set; }
+
+		/// <summary>
+		/// If true, the accounting session is the owner of <see cref="DomainContainer"/>
+		/// and will dispose it upon <see cref="Dispose"/>.
+		/// </summary>
+		public bool OwnsDomainContainer { get; private set; }
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// Restore preexisting entity listeners of <see cref="DomainContainer"/>
+		/// and, if <see cref="OwnsDomainContainer"/> is true, dispose it.
+		/// </summary>
+		public void Dispose()
+		{
+			if (this.DomainContainer != null)
+			{
+				this.DomainContainer.EntityListeners.Clear();
+
+				foreach (var preexistingEntityListener in preexistingEntityListeners)
+				{
+					this.DomainContainer.EntityListeners.Add(preexistingEntityListener);
+				}
+
+				if (this.OwnsDomainContainer)
+				{
+					this.DomainContainer.Dispose();
+				}
+
+				this.DomainContainer = null;
+			}
+		}
+
+		#endregion
+
+		#region Private methods
+
+		private void Initialize(D domainContainer, U agent)
+		{
+			this.DomainContainer = domainContainer;
+			this.Agent = agent;
+
+			this.preexistingEntityListeners = domainContainer.EntityListeners.ToArray();
+			domainContainer.EntityListeners.Clear();
+			domainContainer.EntityListeners.Add(new EntityListener(agent.ID));
+		}
+
+		#endregion
+	}
+}
